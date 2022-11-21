56ba65278392326950fe0e7180f43e1b66f32711
Kayla Smith <kaylainboston@gmail.com>
2022-11-21 09:20:05 -0500

    render.com and access to user pieces

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -2,10 +2,12 @@ class Api::V1::UsersController < ApiController
 
   def index
     render json: User.all
+    # maybe iterate over users to get jewelry info?
   end
   
   def show
     render json: User.all
-
   end
+  
+
 end
